7893cba9a37aadbd6ba3bb4231b62d64da607e55
Matheus Bissoli <matheus.bissoli@usemobile.xyz>
2022-09-09 08:41:54 -0300

    feat: shape added

--- app/src/main/java/com/br/usemobile/android_compose/MainActivity.kt
+++ app/src/main/java/com/br/usemobile/android_compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.br.usemobile.android_compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,6 +19,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -36,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MessageCard(Message("Bife", "Débora"))
+                    MessageCard(Message("nome1", "nome2"))
                 }
             }
         }
@@ -51,29 +53,35 @@ fun MessageCard(msg: Message) {
             painter = painterResource(id = R.drawable.picture1),
             contentDescription = "First Image",
             modifier = Modifier
-                .size(200.dp)
+                .size(40.dp)
                 .clip(CircleShape)
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+                .align(Alignment.CenterVertically)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column() {
-            Text(text = msg.name, color = MaterialTheme.colors.secondaryVariant)
-            
+            Text(text = msg.name, color = MaterialTheme.colors.secondaryVariant, style = MaterialTheme.typography.subtitle2)
+
             Spacer(modifier = Modifier.height(4.dp))
-            
-            Text(text = msg.otherName, color = MaterialTheme.colors.primary)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(text = msg.otherName, modifier = Modifier.padding(all = 4.dp), style = MaterialTheme.typography.body2)
+            }
         }
 
     }
 
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
-    AndroidcomposeTheme {
-        MessageCard(Message("Bife", "Débora"))
-    }
+    MessageCard(Message("nome1", "nome2"))
 }
